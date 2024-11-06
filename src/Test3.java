3ba729cb854801903bca9f394d2209952ed0cda3
kaviarasu1234 <kaviarasu.s@dxc.com>
2024-11-06 14:33:40 +0530

    new

---
+++ src/Test3.java
@@ -0,0 +1,11 @@
+public class Test3 {	
+	public static void main(String args[]) {
+		//
+		String hungry = "yes";
+		if(hungry == "yes" || hungry == "y" || hungry == "Y")
+			System.out.println("eat something....");
+		else {
+			System.out.println("Do work...");
+		}
+	}
+}
