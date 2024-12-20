9af0e871331b2d812fea11e3a9213d481fd1d296
Kaviarasu S <75796035+kaviarasu841@users.noreply.github.com>
2024-12-20 11:41:28 +0530

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,7 @@
 public class Test1 {	
 	public static void main(String args[]) {
 		String hungry = "yes";
-		if(hungry == "yes" || hungry == "y" || hungry == "YES")
+		if(hungry == "yes" || hungry == "y" || hungry == "NO")
 			System.out.println("eat something....");
 		else {
 			System.out.println("Do work...");
